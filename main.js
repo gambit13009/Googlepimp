a63d8f32d121a382128fce48709e99fb370874b5
Jerome David <36279261+gambit13009@users.noreply.github.com>
2018-04-05 22:11:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function ShowDiv() {
+    document.getElementById("myDIV").style.display = "";
+    document.getElementById("BB").style.display='none';
+}
+
+window.onload = function() {
+  document.getElementById('btn').onclick = function()
+  {
+    window.location.href = "file://home/david/Googlepimp/index.html"+document.getElementById("url").value
+  };
+}
+
+setTimeout(function(){ alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000);
